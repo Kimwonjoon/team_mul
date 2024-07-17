099aaa0d51f89ff01947ff73f69186f98914e9ee
Kimwonjoon <kimpass189@gmail.com>
2024-07-17 10:24:50 +0900

    add

--- src/team_mul/mul.py
+++ src/team_mul/mul.py
@@ -1,2 +1,9 @@
-def mul():
-    print(99*99)
+import sys
+def mul(a,b):
+    print(a*b)
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    mul(a,b)
+if __name__ == "__main__":
+    main()
